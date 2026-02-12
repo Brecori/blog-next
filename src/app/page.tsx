faa9607953832a18ed8eb8e180f37acd07190968
breno <breno_tosi@hotmail.com>
2026-02-11 22:11:21 -0300

    feat: configurando tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <h1 className="text-6xl font-bold text-blue-500 hover:text-white hover:bg-blue-500 transition duration-300">
+        Home Page
+      </h1>
+    </div>
+  );
 }
